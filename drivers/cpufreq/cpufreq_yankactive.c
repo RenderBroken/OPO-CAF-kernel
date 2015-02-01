ee1e364426793aef522d6b282612aabcb05546d2
Pranav Vashi <neobuddy89@gmail.com>
2015-02-01 23:06:15 +0530

    cpufreq: yankactive: Tune default parameters
    * Tone down hispeed freq to match our CPU Boost freq,
      lower power consumption and improve target freq scaling.
    * Disabled Sync freq and upthreshold freq to lower
      power consumption.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Zachariah <zkennedy87@gmail.com>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_yankactive.c

--- drivers/cpufreq/cpufreq_yankactive.c
+++ drivers/cpufreq/cpufreq_yankactive.c
@@ -32,7 +32,10 @@
 #include <linux/kernel_stat.h>
 #include <asm/cputime.h>
 
-#define DEFAULT_HISPEED_FREQ	1728000
+#define DEFAULT_HISPEED_FREQ		1190400
+#define DEFAULT_SYNC_FREQ		0
+#define DEFAULT_UP_THRESHOLD_FREQ	0
+#define DEFAULT_UP_THRESHOLD_LOAD	0
 
 static int active_count;
 
@@ -67,7 +70,7 @@ static spinlock_t speedchange_cpumask_lock;
 static struct mutex gov_lock;
 
 /* Hi speed to bump to from lo speed when load burst (default max) */
-static unsigned int hispeed_freq = 1574400;
+static unsigned int hispeed_freq = DEFAULT_HISPEED_FREQ;
 
 /* Go to hi speed when CPU load at or above this value. */
 #define DEFAULT_GO_HISPEED_LOAD 99
@@ -77,7 +80,7 @@ static unsigned long go_hispeed_load = DEFAULT_GO_HISPEED_LOAD;
 static unsigned int sampling_down_factor = 1;
 
 /* Target load.  Lower values result in higher CPU speeds. */
-#define DEFAULT_TARGET_LOAD 95
+#define DEFAULT_TARGET_LOAD 90
 static unsigned int default_target_loads[] = {DEFAULT_TARGET_LOAD};
 static spinlock_t target_loads_lock;
 static unsigned int *target_loads = default_target_loads;
@@ -86,7 +89,7 @@ static int ntarget_loads = ARRAY_SIZE(default_target_loads);
 /*
  * The minimum amount of time to spend at a frequency before we can ramp down.
  */
-#define DEFAULT_MIN_SAMPLE_TIME (20 * USEC_PER_MSEC)
+#define DEFAULT_MIN_SAMPLE_TIME (80 * USEC_PER_MSEC)
 static unsigned long min_sample_time = DEFAULT_MIN_SAMPLE_TIME;
 
 /*
@@ -131,9 +134,9 @@ static bool io_is_busy = 0;
  * up_threshold_any_cpu_freq then do not let the frequency to drop below
  * sync_freq
  */
-static unsigned int up_threshold_any_cpu_load = 80;
-static unsigned int sync_freq = 1574400;
-static unsigned int up_threshold_any_cpu_freq = 1574400;
+static unsigned int up_threshold_any_cpu_load = DEFAULT_UP_THRESHOLD_LOAD;
+static unsigned int sync_freq = DEFAULT_SYNC_FREQ;
+static unsigned int up_threshold_any_cpu_freq = DEFAULT_UP_THRESHOLD_FREQ;
 
 static void cpufreq_yankactive_timer_resched(
 	struct cpufreq_yankactive_cpuinfo *pcpu)
