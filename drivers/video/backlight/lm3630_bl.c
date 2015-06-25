35b08e1e309807bc4f096279d1a2ab54bf1f5f7b
Zachariah <zkennedy87@gmail.com>
2015-06-25 15:43:57 -0400

    State_Notifier: Update Function for Backlight Control
    Signed-off-by: Zachariah <zkennedy87@gmail.com>

--- drivers/video/backlight/lm3630_bl.c
+++ drivers/video/backlight/lm3630_bl.c
@@ -435,11 +435,11 @@ static void lm3630_backlight_unregister(struct lm3630_chip_data *pchip)
 #ifdef CONFIG_STATE_NOTIFIER
 	// if display is switched off
 	if (!use_fb_notifier && bl_level == 0)
-		state_notifier_call_chain(STATE_NOTIFIER_SUSPEND, NULL);
+		state_suspend();
 
 	// if display is switched on
 	if (!use_fb_notifier && bl_level != 0 && pre_brightness == 0)
-		state_notifier_call_chain(STATE_NOTIFIER_ACTIVE, NULL);
+		state_resume();
 #endif
 
 #ifdef CONFIG_MACH_MSM8974_14001
