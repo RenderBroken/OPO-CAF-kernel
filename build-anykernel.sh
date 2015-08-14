79cf4b38e5dced8729786b43aadd1cae61370230
Zachariah <zkennedy87@gmail.com>
2015-08-13 23:10:47 -0400

    Build: Update Toolchains
    Signed-off-by: Zachariah <zkennedy87@gmail.com>

--- build-anykernel.sh
+++ build-anykernel.sh
@@ -37,7 +37,7 @@ REPACK_DIR="${HOME}/android/source/kernel/OPO-AnyKernel"
 PATCH_DIR="${HOME}/android/source/kernel/OPO-AnyKernel/patch"
 MODULES_DIR="${HOME}/android/source/kernel/OPO-AnyKernel/modules"
 ZIP_MOVE="${HOME}/android/source/zips/opo-caf-zips"
-ZIMAGE_DIR="${HOME}/android/source/kernel/msm8974_OPO-CAF_render_kernel/arch/arm/boot"
+ZIMAGE_DIR="${HOME}/android/source/kernel/OPO-CAF-kernel/arch/arm/boot"
 
 # Functions
 function checkout_branches {
@@ -87,14 +87,14 @@ echo "Render Kernel Creation Script:"
 echo -e "${restore}"
 
 echo "Pick Toolchain..."
-select choice in UBER-4.9-Cortex-a15 UBER-5.1
+select choice in UBER-4.9-Cortex-a15 UBER-5.2
 do
 case "$choice" in
 	"UBER-4.9-Cortex-a15")
-		export CROSS_COMPILE=${HOME}/android/source/toolchains/UBER-arm-eabi-4.9-cortex-a15-062715/bin/arm-eabi-
+		export CROSS_COMPILE=${HOME}/android/source/toolchains/UBER-arm-eabi-4.9-cortex-a15-08062015/bin/arm-eabi-
 		break;;
-	"UBER-5.1")
-		export CROSS_COMPILE=${HOME}/android/source/toolchains/UBER-arm-eabi-5.1-062715/bin/arm-eabi-
+	"UBER-5.2")
+		export CROSS_COMPILE=${HOME}/android/source/toolchains/UBER-arm-eabi-5.2-08062015/bin/arm-eabi-
 		break;;
 esac
 done
