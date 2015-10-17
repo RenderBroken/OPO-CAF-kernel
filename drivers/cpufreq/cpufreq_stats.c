dae714857ee68e462a81e507b068fe0602635cfe
Zachariah <zkennedy87@gmail.com>
2015-10-17 15:17:46 +0100

    Revert "sched: cpufreq: Adds a field cpu_power in the task_struct"
    This reverts commit 7fed576f97ef9c00401102bef51809635d5310d3.

--- drivers/cpufreq/cpufreq_stats.c
+++ drivers/cpufreq/cpufreq_stats.c
@@ -20,7 +20,6 @@
 #include <linux/kobject.h>
 #include <linux/spinlock.h>
 #include <linux/notifier.h>
-#include <linux/sched.h>
 #include <asm/cputime.h>
 
 static spinlock_t cpufreq_stats_lock;
@@ -92,24 +91,6 @@ static ssize_t show_time_in_state(struct cpufreq_policy *policy, char *buf)
 	}
 	return len;
 }
-
-void acct_update_power(struct task_struct *task, cputime_t cputime) {
-	struct cpufreq_power_stats *powerstats;
-	struct cpufreq_stats *stats;
-	unsigned int cpu_num, curr;
-
-	if (!task)
-		return;
-	cpu_num = task_cpu(task);
-	powerstats = per_cpu(cpufreq_power_stats, cpu_num);
-	stats = per_cpu(cpufreq_stats_table, cpu_num);
-	if (!powerstats || !stats)
-		return;
-
-	curr = powerstats->curr[stats->last_index];
-	task->cpu_power += curr * cputime_to_usecs(cputime);
-}
-EXPORT_SYMBOL_GPL(acct_update_power);
 
 #ifdef CONFIG_CPU_FREQ_STAT_DETAILS
 static ssize_t show_trans_table(struct cpufreq_policy *policy, char *buf)
